e3d3deb86a742b467b41c8569b08fde4f976d038
bengelhaupt <code@bengelhaupt.com>
2021-07-09 00:03:47 +0200

    implement more

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,7 +1,10 @@
 package de.hpi.spark_tutorial
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.collect_set
+import org.apache.spark.sql.functions.{collect_set, flatten}
+
+import scala.collection.convert.ImplicitConversions.`list asScalaBuffer`
+import scala.collection.mutable
 
 object Sindy {
 
@@ -31,7 +34,7 @@ object Sindy {
         dataframe
           .groupBy("_1")
           .agg(
-            collect_set("_2")
+            collect_set("_2").alias("_2")
           )
       } // List of DataFrames of (value, columns[])
 
@@ -39,19 +42,48 @@ object Sindy {
       .reduce(_ union _)
       .groupBy("_1")
       .agg(
-        collect_set("collect_set(_2)")
+        flatten(collect_set("_2")).alias("_2")
       )
+      .drop("_1") // List of DataFrames of (columns[])
 
-    val attribute_groups = joined_dataframes
-      .map { row =>
-        row.get(1).toString
+    val inclusion_lists = joined_dataframes
+      .flatMap { row =>
+        val elements: List[String] = row.getList(0).toList
+        elements.map { e =>
+          (e, elements.filter(_ != e))
+        }
       }
 
-    attribute_groups.show()
-    /*aggregated_dataframes.foreach { it =>
-      it.show()
-    }*/
+    val aggregated_lists = inclusion_lists
+      .groupBy("_1")
+      .agg(
+        collect_set("_2").alias("_2")
+      )
 
+    val intersected = aggregated_lists
+      .map { row =>
+        val elements: List[mutable.WrappedArray[String]] = row.getList(1).toList
+        val intersection = elements.head.toList
+        elements.drop(1).foreach {
+          intersection.intersect(_)
+        }
+        (row.getString(0), intersection)
+      }
 
+    joined_dataframes.show(50, false)
+
+    val inds = intersected
+      .filter { row =>
+        row._2.nonEmpty
+      }
+      .sort("_1")
+
+    inds.map { row =>
+      row._1 + " < " + row._2.mkString(", ")
+    }
+      .collectAsList()
+      .foreach {
+        println(_)
+      }
   }
 }
